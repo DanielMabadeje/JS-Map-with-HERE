2ee9d756fa019a3e0bc502c96634b5aee2567c38
Daniel <mabadejedaniel1@gmail.com>
2020-06-26 10:09:47 -0700

    first commit

---
+++ template/js/config.js
@@ -0,0 +1,13 @@
+const center = { 
+   lat: 52.5159, 
+   lng: 13.3777, 
+   text: 'Berlin, Germany'
+}
+
+const hereCredentials = {
+   id: 'Kam585TI2ouBsnALAwIZ',
+   code: 'YOUR-HERE-CODE',
+   apikey: 'Y97eSyBivm8e3yKOkm93B87hKwbKHfXsJjngdbFaO_s'
+}
+
+export { center, hereCredentials };
